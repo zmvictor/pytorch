422d03474210cfe367b144d42721b7fc23bf9ea3
Rohan Varma <rvarm1@fb.com>
2021-09-01 16:34:09 -0700

    [c10d] Bring down PG NCCL on failure in abortTimedOutCollectives
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64242
    
    It appears that this is only run in async error handling mode, so
    there is no need to wait for the work cleanup thread to run and handle this
    error if we know the work object encountered an error. We might as well bring
    the process down in this case which is what the work cleanup loop would do
    anyways. The purpose of doing it here is because occasionally other collectives
    may run before the work cleanup loop does, resulting in confusing NCCL error
    messages/crashes when we actually just want to bring down the process.
    ghstack-source-id: 137252096
    
    Differential Revision: [D30659941](https://our.internmc.facebook.com/intern/diff/D30659941/)

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -28,6 +28,7 @@ constexpr const char* const kNCCLAbortedCommStoreKey = "NCCLABORTEDCOMM";
 
 namespace {
 
+const char* handleNCCLGuardLogStr = "ProcessGroupNCCL::WorkNCCL::handleNCCLGuard";
 constexpr int kBytes = 8;
 
 // RAII helper class to manage NCCL group API and CUDA free mutex.
@@ -323,6 +324,8 @@ void ProcessGroupNCCL::WorkNCCL::handleNCCLGuard() {
   std::lock_guard<std::mutex> lock(mutex_);
   if (exception_) {
     auto exceptionMsg = c10::str(
+        handleNCCLGuardLogStr,
+        ": ",
         "Some NCCL operations have failed or timed out. Due to the ",
         "asynchronous nature of CUDA kernels, subsequent GPU operations ",
         "might run on corrupted/incomplete data. To avoid this inconsistency, ",
@@ -583,6 +586,11 @@ void ProcessGroupNCCL::abortTimedOutCollectives(
         ncclComm->ncclCommAbort(exceptionMsg);
         abortedCommIds.emplace(buildNcclUniqueIdStr(ncclComm->getNcclId()));
       }
+      // Handle Exceptions on failed GPU operations by bringing the process
+      // down.
+      if (!terminateProcessGroup_.load()) {
+        work.handleNCCLGuard();
+      }
     }
   }
 }
@@ -594,9 +602,16 @@ void ProcessGroupNCCL::ncclCommWatchdog() {
     LOG(INFO) << "[Rank " << rank_
               << "] NCCL watchdog thread terminated normally";
   } catch (std::exception& e) {
+    auto err = e.what();
     LOG(INFO) << "[Rank " << rank_
               << "] NCCL watchdog thread terminated with exception: "
-              << e.what();
+              << err;
+    // Errors containing handleNCCLGuardLogStr indicate that the
+    // process is unhealthy and needs to be taken down.
+    if (strstr(err, handleNCCLGuardLogStr) != nullptr) {
+        throw;
+    }
+
   } catch (...) {
     LOG(INFO) << "[Rank " << rank_
               << "] NCCL watchdog thread terminated with unknown exception";
