9cb34f6bea74b3117b2346556fab3309a55c5269
Eli Uriegas <eliuriegas@fb.com>
2021-10-19 17:43:42 -0500

    ci: Move windows to build bdist_wheel
    Also moves the miniconda install to only happen if / when Miniconda3 is
    not already installed
    
    Signed-off-by: Eli Uriegas <eliuriegasfb.com>
    
    ghstack-source-id: 918d3e15ec116ed11ce3e9b936565e0e95cc1e8d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/65587
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .jenkins/pytorch/win-test-helpers/installation-helpers/install_miniconda3.bat
+++ .jenkins/pytorch/win-test-helpers/installation-helpers/install_miniconda3.bat
@@ -4,13 +4,14 @@ if "%BUILD_ENVIRONMENT%"=="" (
   set CONDA_PARENT_DIR=C:\Jenkins
 )
 if "%REBUILD%"=="" (
-  IF EXIST %CONDA_PARENT_DIR%\Miniconda3 ( rd /s /q %CONDA_PARENT_DIR%\Miniconda3 )
-  curl --retry 3 -k https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
-  if errorlevel 1 exit /b
-  if not errorlevel 0 exit /b
-  %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
-  if errorlevel 1 exit /b
-  if not errorlevel 0 exit /b
+  IF NOT EXIST %CONDA_PARENT_DIR%\Miniconda3 (
+    curl --retry 3 -k https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
+    if errorlevel 1 exit /b
+    if not errorlevel 0 exit /b
+    %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
+    if errorlevel 1 exit /b
+    if not errorlevel 0 exit /b
+  )
 )
 call %CONDA_PARENT_DIR%\Miniconda3\Scripts\activate.bat %CONDA_PARENT_DIR%\Miniconda3
 if "%REBUILD%"=="" (
