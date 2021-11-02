04072c50acf3001fc4c5de67ecd6f7b471efef4a
soulitzer <soulitzer@gmail.com>
2021-11-02 13:11:44 -0400

    Add forward AD support for miscellanous operators
    ghstack-source-id: 177550ce0bd3098e1ef5be40b9845bcacd8df847
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67040

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -8695,7 +8695,8 @@ op_db: List[OpInfo] = [
            dtypesIfCPU=all_types_and(torch.bfloat16),
            dtypesIfCUDA=all_types_and(torch.half, torch.bfloat16),
            sample_inputs_func=sample_inputs_nn_activation_relu,
-           supports_out=False),
+           supports_out=False,
+           supports_forward_ad=True),
     OpInfo('nn.functional.conv_transpose2d',
            aten_name='conv_transpose2d',
            aliases=('conv_transpose2d',),
@@ -9164,7 +9165,8 @@ op_db: List[OpInfo] = [
                    aliases=('negative', ),
                    ref=np.negative,
                    dtypes=all_types_and_complex_and(torch.half, torch.bfloat16),
-                   assert_autodiffed=True,),
+                   assert_autodiffed=True,
+                   supports_forward_ad=True),
     OpInfo('dist',
            op=torch.dist,
            dtypes=floating_and_complex_types_and(torch.half, torch.bfloat16),
@@ -10349,6 +10351,7 @@ op_db: List[OpInfo] = [
            dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16),
            sample_inputs_func=sample_inputs_stack,
            assert_autodiffed=True,
+           supports_forward_ad=True,
            skips=(
                # TODO: see https://github.com/pytorch/pytorch/issues/64709
                DecorateInfo(unittest.skip("Skipped!"), 'TestCommon', 'test_out'),
