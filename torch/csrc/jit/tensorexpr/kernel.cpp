c890bad3bd284ad5e693fb59e3c448d773d3431a
Mikhail Zolotukhin <mvz@fb.com>
2021-11-02 17:06:11 -0700

    Add a way to define target triple/cpu/attrs for llvm codegen.
    ghstack-source-id: 92cc0fa3791bad22b8cc824fb3168feea8df29fd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66527

--- torch/csrc/jit/tensorexpr/kernel.cpp
+++ torch/csrc/jit/tensorexpr/kernel.cpp
@@ -1198,7 +1198,7 @@ void TensorExprKernel::compile() {
   }
 
   BackendType backendType = inferBackendTypeFromDevice(device_);
-  StmtPtr stmt = transformLoops(backendType, block);
+  stmt_ = transformLoops(backendType, block);
 
   for (auto c : constants_) {
     bufferArgs_.emplace_back(BufHandle(c.buf));
@@ -1207,10 +1207,15 @@ void TensorExprKernel::compile() {
   // Generate code.
   codegen_ = CreateCodeGen(
       getCodeGenName(backendType),
-      stmt,
+      stmt_,
       bufferArgs_,
       device_,
-      kernel_func_name_);
+      SubgraphUtils::generateNameForGraph(graph_));
+}
+
+void TensorExprKernel::recompile() {
+  codegen_ = CreateCodeGen(
+      "llvm_codegen", stmt_, bufferArgs_, device_, kernel_func_name_);
 }
 
 TensorExprKernel::TensorExprKernel(
