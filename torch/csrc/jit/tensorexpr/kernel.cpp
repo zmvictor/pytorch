415353e5e171ddc90be3ceb2d903a8ca81dbbc41
Mikhail Zolotukhin <mvz@fb.com>
2021-11-12 08:29:39 -0800

    Add a pass for bisecting graphs.
    ghstack-source-id: ed1288417ce6c5b3814a2886d7ea8ae62d43ea2a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66847

--- torch/csrc/jit/tensorexpr/kernel.cpp
+++ torch/csrc/jit/tensorexpr/kernel.cpp
@@ -145,6 +145,17 @@ c10::optional<at::Device> pickDeviceType(const std::shared_ptr<Graph>& graph) {
       }
     }
   }
+  for (auto const& input : graph->inputs()) {
+    if (auto tt = input->type()->cast<TensorType>()) {
+      if (auto inputDevice = tt->device()) {
+        TORCH_INTERNAL_ASSERT(
+            !device || *device == *inputDevice,
+            buildErrorMessage(
+                "Different devices specified for inputs to the fuser."));
+        device = inputDevice;
+      }
+    }
+  }
   TORCH_INTERNAL_ASSERT(
       device,
       buildErrorMessage("Could not find device in fuser graph inputs."));
@@ -917,6 +928,8 @@ BufHandle TensorExprKernel::bindSymbolicShapeInput(
 
 Tensor TensorExprKernel::bindInput(const torch::jit::Value* input) {
   auto const& t = input->type();
+  auto const& outputs = input->owningGraph()->outputs();
+  std::unordered_set<const Value*> outputs_set(outputs.begin(), outputs.end());
   Tensor result(nullptr, nullptr);
   switch (t->kind()) {
     case TypeKind::TensorType: {
@@ -928,7 +941,7 @@ Tensor TensorExprKernel::bindInput(const torch::jit::Value* input) {
         bufferArgs_.emplace_back(bufHandle);
         break;
       }
-      if (isContiguous(input)) {
+      if (isContiguous(input) && !outputs_set.count(input)) {
         BufHandle inBuffer(
             "t" + input_name_map_[input],
             toExprHandles(*tt->sizes().concrete_sizes()),
--- torch/csrc/jit/tensorexpr/tensorexpr_init.cpp
+++ torch/csrc/jit/tensorexpr/tensorexpr_init.cpp
@@ -892,6 +892,7 @@ void initTensorExprBindings(PyObject* module) {
   te.def(
       "replace_list_output_with_tuple",
       &tensorexpr::replaceListOutputWithTuple);
+  te.def("trim_graph", &tensorexpr::trimGraph);
   te.def("set_llvm_target_triple", [](const c10::optional<std::string>& val) {
     tensorexpr::LLVMTargetTriple() = val;
   });
