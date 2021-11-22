b5783abd78867f64e38c3c3e3c687e7519740b2d
zhxchen17 <zhxchen17@fb.com>
2021-11-22 15:33:14 -0800

    [jit] Implement DynamicType for TorchScript runtime.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68136
    
    DynamicType is an extension to existing server JIT types. Today using normal server types on Edge is a bit problematic because in embedded environments we don't need the full spectrum of types but we still build with these unneeded dependencies.
    
    Is it possible to just get rid of unneeded JIT types from Edge builds? It's not easy to do so at this moment. For example, on Edge we don't support Union type, but we have to pull in the dependency of Union type because Optional type is being supported which inherits from Union type, so Union type has to be included in the build. Although we could split Union type and Optional type, it could be argued that the root cause is every time we use anything inheriting from `c10::Type`, we don't have the direct evidence of how much dependency we pull in, because we do virtual calls and we don't know what exactly we're calling with server JIT types. If we don't know, it's highly possible that the linker doesn't know either so it cannot effectively strip unused methods.
    
    To address this problem, one option is to implement a separate `DynamicType` which has simpler behavior and doesn't store different types as different symbols in binary but rather raw data (or "tag"). This could increase the binary size by several KBs, so I included several binary size reductions in the same stack, hoping at least we don't regress the binary size.
    
    Currently `DynamicType` inherits from `c10::Type` because I want to reduce the migration cost of `DynamicType` by making it interfacing with existing server JIT types. In the future `DynamicType` should be implemented as a separate class without relying on `c10::Type` to make things both simpler and leaner.
    ghstack-source-id: 143981323
    
    Differential Revision: [D32264615](https://our.internmc.facebook.com/intern/diff/D32264615/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D32264615/)!

---
+++ aten/src/ATen/core/dynamic_type.cpp
@@ -0,0 +1,167 @@
+#include <ATen/core/dynamic_type.h>
+
+#include <ATen/core/function_schema.h>
+#include <ATen/core/ivalue.h>
+#include <ATen/core/jit_type.h>
+#include <c10/util/Exception.h>
+
+namespace c10 {
+
+namespace {
+bool contains(DynamicType::Tag lhs, uint32_t rhs) {
+  return (static_cast<uint32_t>(lhs) | rhs) == static_cast<uint32_t>(lhs);
+}
+bool contains(DynamicType::Tag lhs, DynamicType::Tag rhs) {
+  return contains(lhs, static_cast<uint32_t>(rhs));
+}
+} // namespace
+
+DynamicType::Arguments::Arguments(c10::ArrayRef<TypePtr> args) {
+  elems.reserve(args.size());
+  for (const auto& arg : args) {
+    elems.emplace_back(create(*arg));
+  }
+}
+
+DynamicType::Arguments::Arguments(const c10::FunctionSchema& schema, c10::ArrayRef<TypePtr> args)
+    : Arguments(args) {
+  TORCH_INTERNAL_ASSERT(schema.arguments().size() == args.size());
+  for (size_t i = 0; i < args.size(); i++) {
+    elems[i].label = schema.arguments()[i].name();
+  }
+}
+
+DynamicType::~DynamicType() {
+  if (tag_ == Tag::Singleton) {
+    return;
+  }
+
+  if (tag_ == Tag::Class) {
+    class_.~ClassTypePtr();
+    return;
+  }
+
+  arguments_.~Arguments();
+}
+
+std::shared_ptr<const DynamicType> DynamicType::create(const Type& other) {
+  if (auto dyn = other.cast<DynamicType>()) {
+    return dyn;
+  }
+  return std::shared_ptr<const DynamicType>(new DynamicType{other});
+}
+
+DynamicTypePtr DynamicType::create(Type& other) {
+  if (auto dyn = other.cast<DynamicType>()) {
+    return dyn;
+  }
+  return std::shared_ptr<DynamicType>(new DynamicType{other});
+}
+
+DynamicType::DynamicType(const Type& other) : Type(DynamicType::Kind) {
+  auto kind = other.kind();
+  TORCH_INTERNAL_ASSERT(kind != Kind);
+  if (auto cls = other.cast<ClassType>()) {
+    new (&class_) ClassTypePtr(std::move(cls));
+    tag_ = Tag::Class;
+    return;
+  }
+  switch (kind) {
+#define CASE_TYPE(T, _) \
+  case T##Type::Kind:   \
+    tag_ = Tag::T;      \
+    break;
+    FORALL_DYNAMIC_JIT_TYPES(CASE_TYPE)
+#undef CASE_TYPE
+    default:
+      if (kind == DeviceObjType::Kind || kind == StreamObjType::Kind ||
+          kind == CapsuleType::Kind) {
+        tag_ = Tag::Singleton;
+        typeKind_ = kind;
+        return;
+      }
+      TORCH_INTERNAL_ASSERT(false, "Unsupported dynamic type: ", other.str());
+  }
+
+  auto args = other.containedTypes();
+  if (args.empty()) {
+    new (&arguments_) Arguments();
+    return;
+  }
+
+  if (auto tup = other.castRaw<TupleType>()) {
+    if (auto schema = tup->schema()) {
+      new (&arguments_) Arguments(*schema, args);
+      return;
+    }
+  }
+
+  new (&arguments_) Arguments(args);
+}
+
+bool DynamicType::equals(const DynamicType& other) const {
+  if (this == &other) {
+    return true;
+  }
+  if (tag_ != other.tag_) {
+    return false;
+  }
+  switch (tag_) {
+    case Tag::Class:
+      return *class_ == *other.class_;
+    case Tag::Singleton:
+      return typeKind_ == other.typeKind_;
+    default:
+      return compare(
+          other, [](const LabeledDynamicType& a, const LabeledDynamicType& b) {
+            return a.equals(b);
+          });
+  }
+}
+
+bool DynamicType::operator==(const Type& rhs) const {
+  return equals(*create(rhs));
+}
+
+bool DynamicType::isSubtypeOfExt(const Type& rhs, std::ostream*) const {
+  auto other = create(rhs);
+  if (equals(*other)) {
+    return true;
+  }
+
+  if (contains(other->tag_, tag_)) {
+    return true;
+  }
+
+  if (contains(tag_, 0x80000000)) {
+    if (compare(
+            *other,
+            [](const LabeledDynamicType& a, const LabeledDynamicType& b) {
+              return a.isSubtypeOf(b);
+            })) {
+      return true;
+    };
+  }
+
+  if (other->tag_ == Tag::Optional) {
+    if (isSubtypeOf(other->arguments_.elems[0].ty)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool LabeledDynamicType::isSubtypeOf(const LabeledDynamicType& other) const {
+  if (!other.label || (label == other.label)) {
+    return ty->isSubtypeOf(other.ty);
+  }
+
+  return false;
+}
+
+bool LabeledDynamicType::equals(const LabeledDynamicType& other) const {
+  return (label == other.label) && (*ty == *other.ty);
+}
+
+} // namespace c10
---
+++ aten/src/ATen/core/dynamic_type.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <memory>
+
+#include <ATen/core/class_type.h>
+#include <ATen/core/jit_type_base.h>
+#include <c10/util/Optional.h>
+
+/**
+ * bits       31    30        29    12         11      10      9     1 0 |
+ * covariant | any | singleton | ... | Optional | Class | Dict | ... | Tensor |
+ */
+#define FORALL_DYNAMIC_JIT_TYPES(_) \
+  _(Tensor, 0x00000001)             \
+  _(None, 0x00000002)               \
+  _(Bool, 0x00000004)               \
+  _(Int, 0x00000008)                \
+  _(Float, 0x00000010)              \
+  _(Complex, 0x00000020)            \
+  _(Number, 0x00000038)             \
+  _(String, 0x00000040)             \
+  _(List, 0x00000080)               \
+  _(Tuple, 0x80000100)              \
+  _(Dict, 0x00000200)               \
+  _(Class, 0x00000400)              \
+  _(Optional, 0x80000802)           \
+  _(AnyList, 0x40000080)            \
+  _(AnyTuple, 0xc0000100)           \
+  _(Any, 0xffffffff)
+
+namespace c10 {
+
+struct FunctionSchema;
+struct LabeledDynamicType;
+class DynamicType;
+using DynamicTypePtr = std::shared_ptr<DynamicType>;
+
+// Low dependency jit type with minimal subtyping and structucing support,
+// designed for embedded cases.
+class DynamicType : public Type {
+  using ClassTypePtr = std::shared_ptr<const c10::ClassType>;
+
+ public:
+  ~DynamicType() override;
+
+  struct Arguments {
+    Arguments() = default;
+    Arguments(c10::ArrayRef<TypePtr>);
+    Arguments(const c10::FunctionSchema&, c10::ArrayRef<TypePtr>);
+    std::vector<LabeledDynamicType> elems;
+  };
+
+  enum class Tag : std::uint32_t {
+#define DYNAMIC_TYPE_ITEM(NAME, VAL) NAME = VAL,
+    FORALL_DYNAMIC_JIT_TYPES(DYNAMIC_TYPE_ITEM)
+#undef DYNAMIC_TYPE_ITEM
+        Singleton = 0x20000000,
+  };
+
+  bool operator==(const Type& rhs) const override;
+  bool isSubtypeOfExt(const Type& rhs, std::ostream* why_not) const override;
+  std::string str() const override {
+    return "Dynamic";
+  }
+  static const TypeKind Kind = TypeKind::DynamicType;
+
+ private:
+  friend struct Type;
+  static DynamicTypePtr create(Type& ty);
+  static std::shared_ptr<const DynamicType> create(const Type& ty);
+  DynamicType(const Type& other);
+  bool equals(const DynamicType& other) const;
+
+  template <typename F>
+  bool compare(const DynamicType& other, F&& f) const {
+    if (arguments_.elems.size() != other.arguments_.elems.size()) {
+      return false;
+    }
+    for (size_t i = 0; i < arguments_.elems.size(); i++) {
+      if (!f(arguments_.elems[i], other.arguments_.elems[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Tag tag_;
+  union {
+    Arguments arguments_;
+    ClassTypePtr class_;
+    TypeKind typeKind_;
+  };
+};
+
+struct LabeledDynamicType {
+  c10::optional<std::string> label;
+  DynamicTypePtr ty;
+  explicit LabeledDynamicType(DynamicTypePtr t) : ty(std::move(t)) {}
+
+  bool equals(const LabeledDynamicType& other) const;
+  bool isSubtypeOf(const LabeledDynamicType& other) const;
+};
+
+} // namespace c10
