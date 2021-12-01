67ce8fa4f6112479ffb6f5b9de67c86dfabcb627
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-01 13:42:22 -0800

    Populate runtime with upgrader graph
    ghstack-source-id: 63336a807e4511cd25b3a636ab50e97b4c79056d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68773

---
+++ torch/csrc/jit/operator_upgraders/upgraders.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <c10/macros/Export.h>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+namespace torch {
+namespace jit {
+
+struct UpgradersMap {
+    static std::unordered_map<std::string, std::string> content;
+    static std::mutex *lock;
+    static bool isPopulated;
+};
+
+TORCH_API void populate_upgraders_map(const std::unordered_map<std::string, std::string>& content);
+
+TORCH_API int get_upgraders_map_size();
+
+// this is used for testing, so copying is not a perf issue
+TORCH_API std::unordered_map<std::string, std::string> dump_upgraders_map();
+
+} // namespace jit
+} // namespace torch
